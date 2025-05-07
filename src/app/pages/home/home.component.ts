106be8eae9715452320d03866ae91f268c29603f
unknown <alorentiar03@gmail.com>
2025-05-07 16:54:21 +0700

    update isi konten

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  images: string[] = [
+    'assets/images1.jpg',
+    'assets/images2.jpg',
+    'assets/images3.jpg'
+  ];
+  currentIndex = 0;
 
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
